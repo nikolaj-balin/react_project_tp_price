312e01d372493607b659c55779591d9ec60af397
nikolaj <nikolaj-balin@yandex.ru>
2019-12-12 13:41:44 +0300

    begin_project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,6 @@ const App = () => (
         <div>
             <h1>Теплицы Почтой!</h1>
             <div>
-                <img src={require('./images/1.jpg').default} alt="" />
             </div>
         </div>
     );
