7c67d466fb205cdd672d25c5f6e1a8eee87ecc9b
nikolaj <nikolaj-balin@yandex.ru>
2019-12-15 23:46:53 +0300

    Webpack settings dev

--- webpack.build.conf.js
+++ webpack.build.conf.js
@@ -1,10 +1,77 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
     // BUILD config
     mode: 'production',
-    plugins: []
+    devtool: "none",
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            }
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        },
+            {
+                test: /\.(jpg|png|ttf|tiff|svg|eot|woff|woff2|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'images',
+                    name: '[name].[ext]'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { config: { path: `./postcss.config.js` } }
+                    }, {
+                        loader: 'sass-loader',
+                    }
+                ]
+            }, {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {  config: { path: `./postcss.config.js` } }
+                    }
+                ]
+            }]
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        hash: false,
+        template: './src/index.html',
+        filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+    })
+]
 });
 
 module.exports = new Promise((resolve, reject) => {
--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,6 +1,8 @@
-const webpack =  require('webpack')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
+const webpack =  require('webpack');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.conf');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   // DEV config
@@ -14,11 +16,65 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       errors: true
     }
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+      {
+        test: /\.(jpg|png|ttf|tiff|svg|eot|woff|woff2|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images',
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+          }, {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+          }
+        ]
+      }]
+  },
   plugins: [
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map'
-    })
-  ]
+  new HtmlWebpackPlugin({
+    hash: false,
+    template: './src/index.html',
+    filename: 'index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+  }),
+  new webpack.SourceMapDevToolPlugin({
+    filename: "[file].map"
+  })
+]
 })
 
 module.exports = new Promise((resolve, reject) => {
